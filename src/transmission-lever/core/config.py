d149d78426cc70d4cd9c8ce5dd2ed49254db8541
Tom Villegas <tvillega@dcc.uchile.cl>
2024-12-03 17:23:52 -0300

    feat: search for config files in user and system of the machine

--- src/transmission-lever/core/config.py
+++ src/transmission-lever/core/config.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import logging
 
 
 def get_config() -> dict:
@@ -10,7 +11,32 @@ def get_config() -> dict:
     :return: config dictionary
     """
 
-    f = open(os.path.dirname(__file__) + "/../../transmission-lever.json")
+    my_file = __get_file("transmission-lever",
+                         "config.json")
 
-    cfg = json.load(f)
-    return cfg
+    my_json = open(my_file, 'r')
+    return json.load(my_json)
+
+
+def __get_file(dirname: str, filename: str) -> str:
+    """
+    Check if a file exists on the local system
+    :return: string of the config file full path
+    """
+
+    default = os.path.join(os.path.dirname(__file__) + "/../tlever.json")
+
+    supported_dirs = [
+        os.path.expanduser(f"~/.config/{dirname}/{filename}"),# XDG Standard (User)
+        os.path.join(f"/etc/xdg/{dirname}/{filename}"),# XDG Standard (System)
+        os.path.expanduser(f"~/.{dirname}/{filename}"),# Legacy user
+        os.path.join(f"/etc/{dirname}/{filename}")# System-wide
+    ]
+
+    for supported_dir in supported_dirs:
+        if os.path.exists(supported_dir):
+            return supported_dir
+
+    else:
+        logging.warning("Configuration file not found, using default values")
+        return default
